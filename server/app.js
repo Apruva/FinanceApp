1defd3bd405dd68a5c2efe18d26e37945b7aaddd
apruva <apruva@gmail.com>
2021-05-08 23:44:22 +0200

    added JWT access token middleware - need to find a way to handle errors in json

--- server/app.js
+++ server/app.js
@@ -5,6 +5,11 @@ const rateLimit = require('./Middleware/rateLimit');
 const cors = require('cors');
 const morgan = require('morgan');
 const databaseConnect = require('./Utils/databaseConnect');
+
+const helseregionRoute = require('./Routes/helseregionRoute');
+const nasjonaltRoute = require('./Routes/nasjonaltRoute');
+const helseforetakRoute = require('./Routes/helseforetakRoute');
+
 require('dotenv').config();
 
 databaseConnect();
@@ -13,12 +18,13 @@ app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(rateLimit);
+const jwtCheck = require('./Middleware/jwtCheck');
+app.use(jwtCheck);
 
-const nasjonaltRoute = require('./Routes/nasjonaltRoute');
 app.use('/api/v1/nasjonalt', nasjonaltRoute);
-const helseregionRoute = require('./Routes/helseregionRoute');
+
 app.use('/api/v1/helseregion', helseregionRoute);
-const helseforetakRoute = require('./Routes/helseforetakRoute');
+
 app.use('/api/v1/helseforetak', helseforetakRoute);
 
 app.listen(process.env.DEV_PORT, function (err) {
